e2eff0ece5f67756d5ed1befe100c35265a88b0f
AnGwangHo <akh4444@nate.com>
2020-03-04 10:51:13 +0900

    - footer 내용 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -63,7 +63,7 @@ const Layout = (props: LayoutPropsType) => {
       <div id="content">
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()} JunhoBaik, Built with
+          © {new Date().getFullYear()} AnGwangHo, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
