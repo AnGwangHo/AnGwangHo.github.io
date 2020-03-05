d0c442acd9ad1236893d736205b862193dca8395
AnGwangHo <akh4444@nate.com>
2020-03-05 13:39:32 +0900

    - google searchconsole 재적용

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
 
   /** Optional */
   googleAnalytics: 'UA-152733182-2',     // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleSearchConsole: 'SW4DRlT8LGYoa7d9KMvttT7-ffuBagWqjvyY3C8LpPo', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleSearchConsole: 'ajk2O5QdXHDdidvXZAtDcuue5yoWbbeFcu-EGZYATIo', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
   googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
   googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
